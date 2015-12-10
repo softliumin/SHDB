deff3f71839c62d6e922808f24aca85824dec603
sharper <softliumin@gmail.com>
2015-12-10 17:04:59 +0800

    ：joy：  复习jdbc使用为了后面的mybatis源代码分析

--- src/main/java/cc/sharper/db/Test.java
+++ src/main/java/cc/sharper/db/Test.java
@@ -1,8 +1,6 @@
 package cc.sharper.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by liumin3 on 2015/11/17.
@@ -12,18 +10,63 @@ public class Test
     public static void main(String[] args) throws  Exception
     {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/imooc?useUnicode=true&amp;characterEncoding=utf-8",
-                "root","root");
-        String sql = "select * from user";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://101.200.145.47:3306/liumin?useUnicode=true&amp;characterEncoding=utf-8",
+                "root","liumin110");
         Statement st =  conn.createStatement();
+//        String sql = "delete from user where id='999'";
+//        Statement st =  conn.createStatement();
+//        st.execute(sql);
 
-        //删除
-        String sql2 = "delete from person where id='222'";
-        System. out.println( st.execute( sql2));
+//        String sql  = "select * from user";
+//        ResultSet rs =  st.executeQuery(sql);
+//        while(rs.next())
+//        {
+//            String id  = rs.getString("id");
+//            int age = rs.getInt("age");
+//            String first = rs.getString("NICKNAME");
+//            Timestamp date = rs.getTimestamp("CREATED_TIME");
+//
+//            //Display values
+//            System.out.print("ID: " + id);
+//            System.out.print(", Age: " + age);
+//            System.out.print(", NICKNAME: " + first);
+//            System.out.println(" , CREATED_TIME:"+date);
+//        }
+//        rs.close();
 
-        //更新
-        String sql3 = "update person set name='柳敏' where id='111' ";
-        System. out.println( st.execute( sql3));
+
+        String sql = "select * from user where age = ? and nickname=?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1,"18");
+        ps.setString(2,"大壮");
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next())
+        {
+            String id  = rs.getString("id");
+            int age = rs.getInt("age");
+            String first = rs.getString("NICKNAME");
+            Timestamp date = rs.getTimestamp("CREATED_TIME");
+
+            //Display values
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + age);
+            System.out.print(", NICKNAME: " + first);
+            System.out.println(" , CREATED_TIME:"+date);
+        }
+
+
+
+
+
+//        //删除
+//        String sql2 = "delete from person where id='222'";
+//        System. out.println( st.execute( sql2));
+//
+//        //更新
+//        String sql3 = "update person set name='柳敏' where id='111' ";
+//        System. out.println( st.execute( sql3));
+        st.close();
         conn.close();
 
     }
